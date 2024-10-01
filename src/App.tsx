cedaabf2532a686897dd0ecee5fd00c79ed4b351
barakadanny <barakadan421@gmail.com>
2024-10-01 16:46:53 +0100

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card";
+import { products } from "./api/products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Shop here</h1>
+        <div>
+          {products.map((product) => (
+            <Card key={product.id}>
+              <Card.Image src={product.image} />
+              <Card.Name>{product.name}</Card.Name>
+              <Card.Description>{product.description}</Card.Description>
+              <Card.Price>${product.price}</Card.Price>
+              <Card.Summary>{product.summary}</Card.Summary>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h1>Products Overview</h1>
+        <div>
+          {products.map((product) => (
+            <Card key={product.id}>
+              <Card.Image src={product.image} />
+              <Card.Name>{product.name}</Card.Name>
+              <Card.Summary>{product.summary}</Card.Summary>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
