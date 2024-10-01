35d7b7ac396aea698dc13dd46cb66f91107c5fcd
barakadanny <barakadan421@gmail.com>
2024-10-01 16:45:55 +0100

    products api

---
+++ src/api/products.ts
@@ -0,0 +1,27 @@
+// products.ts
+export const products = [
+  {
+    id: 1,
+    image: "https://picsum.photos/200/300",
+    name: "Product 1",
+    price: 19.99,
+    description: "This is a description of product 1.",
+    summary: "This is a summary of product 1.",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/200/300",
+    name: "Product 2",
+    price: 29.99,
+    description: "This is a description of product 2.",
+    summary: "This is a summary of product 2.",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/200/300",
+    name: "Product 3",
+    price: 39.99,
+    description: "This is a description of product 3.",
+    summary: "This is a summary of product 3.",
+  },
+];
